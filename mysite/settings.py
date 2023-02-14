113f430fdb80c3cecb35268f0173f675b9ce5aa2
leeny_the_bear <leeny@inbox.ru>
2023-02-15 00:35:33 +0600

    del f-string

--- mysite/settings.py
+++ mysite/settings.py
@@ -130,7 +130,7 @@ STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 MEDIA_URL = '/'
-MEDIA_ROOT = f'{BASE_DIR}/'
+MEDIA_ROOT = BASE_DIR
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
